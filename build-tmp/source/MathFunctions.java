f1e0bb252c62901f9aa9ab9fd0c85320b5c19e68
ben441318936 <ben441318936@gmail.com>
2014-11-09 17:44:44 -0800

    print functions added

--- build-tmp/source/MathFunctions.java
+++ build-tmp/source/MathFunctions.java
@@ -16,20 +16,40 @@ public class MathFunctions extends PApplet {
 
 public void setup()
 {
+	//examples
 	println(lcosAngleSSS(1,0.5f,Math.sqrt(3)/2));
+	printlcosAngleSSS(1,0.5f,Math.sqrt(3)/2);
 	println(lcosSideSAS(0.5f,90,Math.sqrt(3)/2));
-	println(lcosSideASS(90,0.5f,1));
-	println(lsinSideAAS(90,30,1));
+	printlcosSideSAS(0.5f,90,Math.sqrt(3)/2);
+	println(lcosSideASS(30,1,0.5f));
+	printlcosSideASS(30,1,0.5f);
+	println(lsinSideAAS(90,60,1));
+	printlsinSideAAS(99,60,1);
+	println(lsinSideASA(30,1,60));
+	printlsinSideASA(30,1,60);
 }
+//trigonometry
 public double lcosAngleSSS(double a, double b, double c) //using Law of Cosine to find missing angle A
 {
-	return Math.toDegrees(Math.acos((Math.pow(a,2)-Math.pow(b,2)-Math.pow(c,2))/(-2.0f*b*c)));
+	double d=Math.toDegrees(Math.acos((Math.pow(a,2)-Math.pow(b,2)-Math.pow(c,2))/(-2.0f*b*c)));
+	return d;
+}
+public void printlcosAngleSSS(double a, double b, double c)
+{
+	double d=lcosAngleSSS(a,b,c);
+	println(String.format("%.3f",d)); 
 }
 public double lcosSideSAS(double a, double C, double b) //using Law of Cosine to find missing side C
 {
-	return Math.sqrt((Math.pow(a,2)+Math.pow(b,2))-(2*a*b*Math.cos(Math.toRadians(C))));
+	double d=Math.sqrt((Math.pow(a,2)+Math.pow(b,2))-(2*a*b*Math.cos(Math.toRadians(C))));
+	return d;
 }
-public double [] lcosSideASS(double A, double a, double b) //using Law of Cosine to find missing side c
+public void printlcosSideSAS(double a, double C, double b)
+{
+	double d=lcosSideSAS(a,C,b);
+	println(String.format("%.3f",d));
+}
+public double [] lcosSideASS(double A, double b, double a) //using Law of Cosine to find missing side c
 {
 	if(A==90)
 	{
@@ -50,10 +70,39 @@ public double [] lcosSideASS(double A, double a, double b) //using Law of Cosine
 		return roots;
 	}
 }
+public void printlcosSideASS(double A, double b, double a)
+{
+	double[] d=lcosSideASS(A,b,a);
+	for(int i=0;i<d.length;i++)
+	{
+		println(String.format("%.3f",d[i]));
+	}
+}
 public double[] lsinSideAAS(double A, double B, double a) //using Law of Sine to find missing side b and c
 {
 	double[] answers={Math.sin(Math.toRadians(B))*a/Math.sin(Math.toRadians(A)), Math.sin(Math.toRadians(180-B-A))*a/Math.sin(Math.toRadians(A))};
 	return answers;
+}
+public void printlsinSideAAS(double A, double B, double a)
+{
+	double[] d=lsinSideAAS(A,B,a);
+	for(int i=0;i<d.length;i++)
+	{
+		println(String.format("%.3f",d[i]));
+	}
+}
+public double[] lsinSideASA(double A, double b, double C) //using Law of Sine to find missing side a and c
+{
+	double[] answers={Math.sin(Math.toRadians(A))*b/Math.sin(Math.toRadians(180-C-A)),Math.sin(Math.toRadians(C))*b/Math.sin(Math.toRadians(180-C-A))};
+	return answers;
+}
+public void printlsinSideASA(double A, double b, double C)
+{
+	double[] d=lsinSideASA(A,b,C);
+	for(int i=0;i<d.length;i++)
+	{
+		println(String.format("%.3f",d[i]));
+	}
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "MathFunctions" };
